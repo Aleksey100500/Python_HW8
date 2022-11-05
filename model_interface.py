3d344b6d850eac1f4833b7fe2c252ec5a5f409f6
Aleksey <a_zhuravlyov@bk.ru>
2022-11-05 17:02:35 +0300

    Added new func for user updates

--- model_interface.py
+++ model_interface.py
@@ -2,6 +2,8 @@ import csv
 from distutils import dir_util
 import model_except as me
 import csv
+
+
 def user_interface():
     user_menu()
     number = user_choise()
@@ -13,6 +15,9 @@ def user_interface():
         user_choise_work()
     elif number == 4:
         user_choise_new_rab()
+    elif number == 5:
+        user_choise_update_new(user_choise_sotrudnik())
+
 
 def user_menu():
     print('\n Выберите необходимое действие.')
@@ -23,17 +28,19 @@ def user_menu():
     print('5. Обновить данные сотрудника.')
     print('6. Удалить существующую запись.')
     print('7. Закончить работу.\n')
-    
+
 
 def user_choise():
     vibor = me.check_number(input('Выберите пункт меню: '))
     return vibor
+
 
 def user_choise_fullcsv():
     with open('BD_STO.csv', newline='') as csvfile:
         all_reader = csv.reader(csvfile, delimiter=',')
         for row in all_reader:
             print(row)
+
 
 def user_choise_sotrudnik():
     surch_name = me.check_name()
@@ -43,6 +50,8 @@ def user_choise_sotrudnik():
             row['SNFN'] = row['SNFN'].split()
             if row['SNFN'][0] == surch_name:
                 print(row)
+    return surch_name
+
 
 def user_choise_work():
     surch_name = me.check_work()
@@ -53,6 +62,7 @@ def user_choise_work():
             if row['JobTitle'][0] == surch_name:
                 print(row)
 
+
 def surch_col():
     with open('BD_STO.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
@@ -61,35 +71,91 @@ def surch_col():
             colums += 1
         return colums + 1
 
+
 def name_workers():
     new_name = input('Введите ФИО сотрудника: ')
     new_name = new_name.lower()
     new_name = new_name.title()
     return new_name
 
+
 def year_of_birth():
-    year_f = input('Введите дату рождения в формате ДД.ММ.ГГ: ')
+    year_f = input('Введите дату рождения в формате ДД.ММ.ГГГГ: ')
     return year_f
 
+
 def number_of_tel():
-    number_f = input('Введите номер телефона с кодом оператора в формате (375(код)..): ')
+    number_f = input(
+        'Введите номер телефона с кодом оператора в формате (375(код)..): ')
     return number_f
+
 
 def education_workers():
     new_name = input('Введите образование сотрудника: ').lower()
     return new_name
 
+
 def jobtitle_workers():
     new_name = input('Введите должность сотрудника: ').lower()
     return new_name
 
+
 def user_choise_new_rab():
-    with open('BD_STO.csv', newline='') as csvfile:
-        file_writer = csv.writer(csvfile, delimiter=',')
-        # first_row = surch_col()
-        # second_row = name_workers()
-        # file_writer.writerow([surch_col(), name_workers(), year_of_birth(), number_of_tel(), education_workers(), jobtitle_workers()])
-        for row in file_writer:
-            file_writer.writerow(['Null'])
-            print(row)
+    with open('BD_STO.csv', 'a') as csvfile:
+        calc = surch_col()
+        new_name = name_workers()
+        birth = year_of_birth()
+        tel_num = number_of_tel()
+        ed_wor = education_workers()
+        job_tit = jobtitle_workers()
+        csvfile.write('\n{},{},{},{},{},{}'.format(
+            calc, new_name, birth, tel_num, ed_wor, job_tit))
+        print('Записано.')
+
+
+def user_choise_update_new():
+    old_name = user_choise_sotrudnik()
+    print('Что нужно обновить: \n')
+    print('1. Фамилию.')
+    print('2. Год рождения.')
+    print('3. Номер телефона.')
+    print('4. Образование.')
+    print('5. Должность.')
+    with open('BD_STO.csv', 'rt') as csvfile:
+        data = csvfile.read()
+        user_update_menu_1 = user_choise()
+        if user_update_menu_1 == 1:
+            with open('BD_STO.csv', 'wt') as csvfile:
+                new_name = me.check_name()
+                data = data.replace(f'{old_name}', f'{new_name}')
+                csvfile.write(data)
+        elif user_update_menu_1 == 2:
+            old_year = year_of_birth()
+            with open('BD_STO.csv', 'wt') as csvfile:
+                new_year = year_of_birth()
+                data = data.replace(f'{old_year}', f'{new_year}')
+                csvfile.write(data)
+        elif user_update_menu_1 == 3:
+            old_tel = number_of_tel()
+            with open('BD_STO.csv', 'wt') as csvfile:
+                new_tel = number_of_tel()
+                data = data.replace(f'{old_tel}', f'{new_tel}')
+                csvfile.write(data)
+        elif user_update_menu_1 == 4:
+            old_education = education_workers()
+            with open('BD_STO.csv', 'wt') as csvfile:
+                new_education = education_workers()
+                data = data.replace(f'{old_education}', f'{new_education}')
+                csvfile.write(data)
+        elif user_update_menu_1 == 5:
+            old_jobtitle = jobtitle_workers()
+            with open('BD_STO.csv', 'wt') as csvfile:
+                new_jobtitle = jobtitle_workers()
+                data = data.replace(f'{old_jobtitle}', f'{new_jobtitle}')
+                csvfile.write(data)
+        else:
+            print('Вы вернулись в гланое меню.')
+        user_interface()
+
+
 user_interface()
